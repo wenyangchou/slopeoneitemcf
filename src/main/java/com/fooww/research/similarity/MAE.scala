96725c18a0c48d7a2da3139cf44e836aa67b3b09
zhouwenyang <wenyang.chou@foxmail.com>
2019-09-09 17:33:10 +0800

    接口重写

---
+++ src/main/java/com/fooww/research/similarity/MAE.scala
@@ -0,0 +1,42 @@
+package com.fooww.research.similarity
+
+import org.apache.spark.rdd.RDD
+
+/**
+  * @author ：zwy
+  */
+object MAE {
+
+  def getMae(trainRDD:RDD[(Long,Long,Double)],testRDD:RDD[(Long,Long,Int)])={
+
+    val train = trainRDD.map(f=>{
+      ((f._1,f._2),f._3)
+    })
+
+    val test = testRDD.map(f=>{
+      ((f._1,f._2),f._3)
+    })
+
+    val subRDD = test.join(train).map(f=>{
+      (f._1,Math.pow(f._2._1 - f._2._2,2) )
+    })
+
+    var count = 0
+    var mae:Double = 0
+
+    subRDD.foreachPartition(partition=>{
+      partition.foreach(f=>{
+        count = count+1
+      })
+    })
+
+    subRDD.foreachPartition(partition=>{
+      partition.foreach(f=>{
+        mae = mae + f._2 /count
+      })
+    })
+
+    mae
+  }
+
+}
--- src/main/java/com/fooww/research/similarity/Recommend.scala
+++ src/main/java/com/fooww/research/similarity/Recommend.scala
@@ -9,34 +9,19 @@ import org.apache.spark.rdd.RDD
 object Recommend {
 
   def recommend(rantRDD:RDD[(Long,Long,Int)],devRDD:RDD[(Long,Long,Double)],itemSimilarityRDD:RDD[(Long,Long,Double)],
-                userAvgSimilarity:RDD[(Long,Double)]): RDD[(Long,List[Long])] ={
-
-    val item_itemSimilarity = itemSimilarityRDD.map(f=>{
-      (f._1,(f._2,f._3))
-    })
-
+                userAvgSimilarity:RDD[(Long,Double)]): RDD[(Long,Long,Double)] ={
     val item_user_rant = rantRDD.map(f=>{
       (f._2,(f._1,f._3))
     })
-
     val item_dev = devRDD.map(f=>{
       (f._1,(f._2,f._3))
     })
-
-    val denominatorRDD = item_user_rant.join(item_itemSimilarity).map(f=>{
-
+    val resultRDD = item_user_rant.join(item_dev).map(f=>{
+      ((f._2._1._1,f._1),f._2._1._2+f._2._2._2)
+    }).reduceByKey(_+_).map(f=>{
+      (f._1._1,f._1._2,f._2)
     })
-
-//    val denominatorRDD = item_user_rant.join(item_itemSimilarity).map(f=>{
-//      (f._2._1._1,f._2._2._2*f._2._1._2)
-//    }).reduceByKey(_+_)
-//
-//    val moleculeRDD = item_dev.join(item_user_rant).map(f=>{
-//      (f._1,f._2._1._2+f._2._2._2)
-//    }).join(item_itemSimilarity).map(f=>{
-//      (f._1,f._2._1*f._2._2._2)
-//    }).join(userAvgSimilarity).map(f=>{
-//      (f)
-//    })
+    resultRDD
   }
+
 }
